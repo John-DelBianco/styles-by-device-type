f0457084b0522a7417b4002390afd530e49c2ae6
John-DelBianco <jdelb95@outlook.com>
2021-10-20 13:35:51 -0400

    Conditional rendering clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,27 @@
 import React from 'react';
-import { MobileView, BrowserView, isDesktop, isMobile, AndroidView, IOSView, isAndroid, isIOS } from 'react-device-detect';
+import { BrowserView, isDesktop, isMobile, AndroidView, IOSView } from 'react-device-detect';
 import './App.css';
 
 function App() {
-  
-  if(isDesktop) {
+
+  if(isMobile || isDesktop) {
     return(
       <div>
+        <AndroidView className="android">
+          <div>AndroidView</div>
+        </AndroidView>
+        <IOSView className="ios">
+          <div>IOSView</div>
+        </IOSView>
         <BrowserView className="browser">
           <div>BrowserView</div>
         </BrowserView>
       </div>
     );
-  }else if(isMobile) {
-    if(isAndroid) {
-      return(
-        <AndroidView className="android">
-          <div>AndroidView</div>
-        </AndroidView>
-      );
-    }else if(isIOS) {
-      return(
-        <IOSView className="ios">
-          <div>IOSView</div>
-        </IOSView>
-      );
-    }else {
-      return(
-        <MobileView className="mobile">
-          <div>MobileView</div>
-        </MobileView>
-      );
-    }
   }else {
     return(
       <div className="not-detected">
-        Device not detected
+        Device wasn't detected
       </div>
     );
   }

16e0d546472eb44835163ffc1a380ffd6b4d3d1a
John Del Bianco <jdelb95@outlook.com>
2021-10-20 12:19:41 -0400

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+import { MobileView, BrowserView, isDesktop, isMobile, AndroidView, IOSView, isAndroid, isIOS } from 'react-device-detect';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  if(isDesktop) {
+    return(
+      <div>
+        <BrowserView className="browser">
+          <div>BrowserView</div>
+        </BrowserView>
+      </div>
+    );
+  }else if(isMobile) {
+    if(isAndroid) {
+      return(
+        <AndroidView className="android">
+          <div>AndroidView</div>
+        </AndroidView>
+      );
+    }else if(isIOS) {
+      return(
+        <IOSView className="ios">
+          <div>IOSView</div>
+        </IOSView>
+      );
+    }else {
+      return(
+        <MobileView className="mobile">
+          <div>MobileView</div>
+        </MobileView>
+      );
+    }
+  }else {
+    return(
+      <div className="not-detected">
+        Device not detected
+      </div>
+    );
+  }
 }
 
 export default App;
